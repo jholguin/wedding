80a9a7387b4e0c94b2afba7073cff05f7d3dac46
Josh Holguin <josh.holguin@springbox.com>
2017-06-07 22:29:14 -0500

    Adding more sections

---
+++ client/components/OurStory/OurStory.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './OurStory.scss'
+
+
+export default class OurStory extends Component{
+
+  render(){
+    
+    return(
+      <section className="container our-story">
+        <h2>How it began</h2>
+        
+      </section>
+    )
+  }
+}
--- client/index.js
+++ client/index.js
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import Couple from './components/Couple/Couple'
+import OurStory from './components/OurStory/OurStory'
 
 ReactDOM.render(
 	<div>
 		<Header />
 		<Home weddingDate="Feburary 24, 2018" />
     <Couple />
+    <OurStory />
 	</div>
 	,document.getElementById('app')
 )
