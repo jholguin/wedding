730f97fdde52ec174cdc4e16140e775583dd8701
Joshua Holguin <jholguin@saatvamattress.com>
2017-08-16 10:42:23 -0500

    Adding admin section for login

--- client/index.js
+++ client/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
 import { HashRouter, Route } from 'react-router-dom'
 import { Provider } from "react-redux"
-import { setLanguages, addTranslation } from 'react-localize-redux';
+import { setLanguages, addTranslation, setActiveLanguage } from 'react-localize-redux';
 import store from "./store"
 import App from './components/App/App'
 import Admin from './components/Admin/Admin'
+
 
 const languages = ['en', 'pt'];
 store.dispatch(setLanguages(languages));
@@ -13,7 +15,29 @@ store.dispatch(setLanguages(languages));
 const pageTranslations = require('./global.locale.json')
 store.dispatch(addTranslation(pageTranslations));
 
+const mapsURL = 'https://maps.googleapis.com/maps/api/geocode/json'
 
+if(navigator.geolocation){
+  navigator.geolocation.getCurrentPosition((pos) => {
+    let { latitude, longitude } = pos.coords
+    axios.get(mapsURL, {
+      params:{
+        latlng: `${latitude},${longitude}`,
+        key: 'AIzaSyAMK5ogq2OPR6epGFUfNg0Yqd78TSI_cpY'
+      }
+    }).then((res) => {
+      let data = res.data.results
+      let newdata = data.filter((elem, index) => {
+        return elem.types[0] == "country"
+      })[0]
+
+      if(newdata.address_components[0]["short_name"]){
+        let country = newdata.address_components[0]["short_name"].toLowerCase()
+        store.dispatch(setActiveLanguage((country == "pt") ? country : 'en'))
+      }
+    })
+  })
+}
 
 ReactDOM.render(
     <Provider store={store}>
